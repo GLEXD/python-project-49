4fd04c0d7532907b67272bf6d02e0512c31630d3
GLEXD <glebushekbur@gmail.com>
2024-10-13 00:12:59 +0300

    Adding 5th step to the repository without asciinema

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
-instruction = 'Answer "yes" if the number is even, otherwise answer "no".'
+from prompt_toolkit.filters import is_multiline
+from logic_of_brain_even import run_game
+def is_even():
+    run_game()
+if __name__ == '__main__':
+    is_even()
--- brain_games/scripts/logic_of_brain_even.py
+++ brain_games/scripts/logic_of_brain_even.py
@@ -1,29 +1,33 @@
 import prompt
 import random
-number = random.randint(1, 100)
-def get_correct_answer():
-    answer = ""
-    if number % 2 == 0:
-        answer = "yes"
-    if number % 2 != 0:
-        answer = "no"
-    return answer
+
+
 def run_game():
+    wins = 0
     name = prompt.string('''Welcome to the Brain Games!
 May I have your name? ''')
     print(f"""Hello, {name}!
 Answer "yes" if the number is even, otherwise answer "no".""")
-    print(f"Question: {number}")
-    user_answer = prompt.string("Your answer: ")
-    if get_correct_answer() == user_answer:
-        print("Correct!")
-    else:
-        print(f"""'{user_answer}' is wrong answer ;(. Correct answer was '{get_correct_answer()}'.
+    for _ in range(3):
+        number = random.randint(1, 100)
+        answer = ""
+        if number % 2 == 0:
+            answer = "yes"
+        if number % 2 != 0:
+            answer = "no"
+        print(f"Question: {number}")
+        user_answer = prompt.string("Your answer: ")
+        if answer == user_answer:
+            print("Correct!")
+            wins += 1
+        else:
+            print(f"""'{user_answer}' is wrong answer ;(. Correct answer was '{answer}'.
 Let's try again, {name}!""")
+            break
+        if wins == 3:
+            print(f"Congratulations, {name}!")
+            break
 
 
-
-
-
-run_game()
-
+if __name__ == '__main__':
+    run_game()
