b1330a8172dad3774c45860b30e10714148aec11
GLEXD <glebushekbur@gmail.com>
2024-10-16 17:30:12 +0300

    Fixing some problems with 5th step, starting project again

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-from prompt_toolkit.filters import is_multiline
 from logic_of_brain_even import run_game
+
+
 def is_even():
     run_game()
+
+
 if __name__ == '__main__':
     is_even()
