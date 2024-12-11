f741f95102014a55b2fc3891fab5206c36943b1e
GLEXD <glebushekbur@gmail.com>
2024-12-11 15:22:29 +0300

    Adding 7th step

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,9 @@
+from .logic_of_brain_gcd import run_gcd_game
+
+
+def main():
+    run_gcd_game()
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/logic_of_brain_gcd.py
+++ brain_games/scripts/logic_of_brain_gcd.py
@@ -1,17 +1,6 @@
 import prompt
 import random
-
-
-# def find_gcd():
-#     num1 = random.randint(1, 100)
-#     num2 = random.randint(1, 100)
-#     question = print(num1, num2)
-#     while num1 != 0 and num2 != 0:
-#         if num1 > num2:
-#             num1 = num1 % num2
-#         else:
-#             num2 = num2 % num1
-#     return num1 + num2, question
+import math
 
 
 def run_gcd_game():
@@ -19,17 +8,17 @@ def run_gcd_game():
     name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
     print(f'Hello, {name}!\nFind the greatest common divisor of given numbers.')
     for _ in range(3):
-        num1 = random.randint(1, 100)
-        num2 = random.randint(1, 100)
+        num1 = random.randint(50, 100)
+        num2 = random.randint(50, 100)
+        print(f"Question: {num1} {num2}")
+        user_answer = prompt.string("Your answer: ")
         while num1 != 0 and num2 != 0:
             if num1 > num2:
                 num1 = num1 % num2
             else:
                 num2 = num2 % num1
         correct_answer = num1 + num2
-        print(f"Question:{num1} {num2}")
-        user_answer = prompt.string("Your answer: ")
-        if correct_answer == user_answer:
+        if correct_answer == int(user_answer):
             print("Correct!")
             wins += 1
         else:
@@ -38,3 +27,7 @@ def run_gcd_game():
         if wins == 3:
             print(f"Congratulations, {name}!")
             break
+
+
+if __name__ == '__main__':
+    run_gcd_game()
